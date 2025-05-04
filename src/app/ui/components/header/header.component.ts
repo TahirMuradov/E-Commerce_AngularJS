33146cbee0c37e29d40a9d213cbf7fd683b599f2
TahirMuradov <muradovtahir01@gmail.com>
2025-05-04 17:36:17 +0400

    HeaderAdded

--- src/app/ui/components/header/header.component.ts
+++ src/app/ui/components/header/header.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, NgClass, NgIf } from '@angular/common';
+import { Component, signal } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [NgClass,NgIf,CurrencyPipe],
+  standalone:true,
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  clickSignal=signal({
+    cartListToogle:false,
+    toggleMenu:false,
+    menuVisible:false
+  })
+  onclick(cartListToogle:boolean,
+    toggleMenu:boolean,
+    menuVisible:boolean,
+  ){
+    this.clickSignal.set({
+ cartListToogle:cartListToogle,
+ menuVisible:menuVisible,
+ toggleMenu:menuVisible
+    })
+
+if (cartListToogle) {
+  document.body.classList.add('cart-data-open');
+} else {
+  document.body.classList.remove('cart-data-open');
+}
+
+
+  }
+
+
 
 }
