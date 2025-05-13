b4024909b18a23ff408790b8aa0b16eebc770176
TahirMuradov <muradovtahir01@gmail.com>
2025-05-13 12:29:10 +0400

    ForgotPassword Form Added themplateDrivenForms

--- src/app/ui/components/auth/forgotpassword/forgotpassword.component.ts
+++ src/app/ui/components/auth/forgotpassword/forgotpassword.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-forgotpassword',
-  imports: [RouterLink],
+  imports: [RouterLink,FormsModule],
+  standalone:true,
   templateUrl: './forgotpassword.component.html',
   styleUrl: './forgotpassword.component.css'
 })
-export class ForgotpasswordComponent {
-
+export class ForgotpasswordComponent implements OnInit {
+  //template-driven Forms
+  @ViewChild("frm",{static:true})frm:NgForm
+  ngOnInit(): void {
+console.log(this.frm)
+console.log(this.frm.form)
+console.log(this.frm.valid)
+console.log(this.frm.value)
+console.log(this.frm.touched)
+console.log(this.frm.submitted)
+console.log(this.frm.controls)
+  }
+onSubmitForm(data:{email:string}){
+console.log(data.email)
+}
 }
