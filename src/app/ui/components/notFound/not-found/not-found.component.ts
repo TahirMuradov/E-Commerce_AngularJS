57ccbcf90556343409a64903ccf78238c248e879
TahirMuradov <muradovtahir01@gmail.com>
2025-05-15 14:54:06 +0400

    Routing Added to NotFoundPage

--- src/app/ui/components/notFound/not-found/not-found.component.ts
+++ src/app/ui/components/notFound/not-found/not-found.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TranslateService ,TranslateModule} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-not-found',
-  imports: [TranslateModule],
+  imports: [TranslateModule,RouterLink],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
