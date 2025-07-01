0fddae022569cb1e49e41346c8f04c472eed58f5
TahirMuradov <muradovtahir01@gmail.com>
2025-07-01 10:15:23 +0400

    Server.ts Added netlifyCommonEngineHandlerMethod

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,21 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 
  const app = express();
 const angularApp = new AngularNodeAppEngine();
+import { CommonEngine } from '@angular/ssr/node'
+import { render } from '@netlify/angular-runtime/common-engine.mjs'
 
+// const commonEngine = new CommonEngine()
+
+export async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
+  // Example API endpoints can be defined here.
+  // Uncomment and define endpoints as necessary.
+  // const pathname = new URL(request.url).pathname;
+  // if (pathname === '/api/hello') {
+  //   return Response.json({ message: 'Hello from the API' });
+  // }
+
+  return await render(angularApp)
+}
 /**
  * Example Express Rest API endpoints can be defined here.
  * Uncomment and define endpoints as necessary.
